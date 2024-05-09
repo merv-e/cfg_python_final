e2f0368bc739f6da77991aa447a40d0879c2349a
merv-e <34655454+merv-e@users.noreply.github.com>
2024-05-09 18:24:15 +0300

    some more stuff and todos are added.

--- deneme.py
+++ deneme.py
@@ -8,3 +8,40 @@ TODOS :
 '''
 
 
+# Learning the index method or function or whatever :D
+'''
+people = ["Merve", "Orhan", "Mualla", "Orkun", "Willy"]
+
+guess = input("Guess if the name exists!" + "\n" + "Write a name: ")
+
+try:
+    print(people.index(guess))
+except ValueError:
+    print("That item does not exist")
+
+# print(people.index("P"))
+'''
+
+
+# filter function
+'''
+ages = [5, 12, 17, 18, 24, 32]
+
+def myFunc(x):
+  if x < 18:
+    return False
+  else:
+    return True
+
+adults = filter(myFunc, ages)
+
+for x in adults:
+  print(x)
+'''
+
+'''
+countries={"Ghana": "Accra", "China": "Beijing"}
+# using the del keyword
+del countries["China"]
+print(countries)
+'''
--- main.py
+++ main.py
@@ -24,14 +24,43 @@ def run():
     ingredient = input('Enter an ingredient: ')
     results = recipe_search(ingredient)
 
-    for result in results:
-        recipe = result['recipe']
-        print(recipe['label'])
-        print(recipe['uri'])
-        print()
+    has_allergy = input("Do you have any allergies? Please answer with a 'yes' with 'y' or 'no' with 'n': ")
+
+    if has_allergy == "y":
+        allergy = input("What are you allergic of? Please write down: ")
+        for result in results:
+           recipe = result["recipe"]
+           try:
+               if recipe["ingredientLines"].index(allergy):
+                   # TODO-1: filter the recipes that the user is not allergic of.
+                   print("index of the allergen:", recipe["ingredientLines"].index(ingredient))
+                   index_of = recipe["ingredientLines"].index(ingredient)
+                   recipe["ingredientLines"].index(ingredient).pop(index_of)
+                   print(index_of)
+
+                   # TODO-2: save it to a file for future use.
+           except ValueError:
+                #  print("Good to go! That allergen is not listed!")
+                # print(recipe)
+                print("Todo bien!")
+
+    else:
+        for result in results:
+            print(result["recipe"]["label"])
+            print(result["recipe"]["uri"])
+
+
+    # def add_recipe_to_favourites(recipe):
+
 
 run()
 
+'''
+# and filter(filter_recipes_by_allergy, allergy)
 
-
-
+def filter_recipes_by_allergy(al):
+    if al:
+        return False
+    else:
+        return True
+'''
