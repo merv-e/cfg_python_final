f3e831970f241f930aae56083ee07c6367672e0c
merv-e <34655454+merv-e@users.noreply.github.com>
2024-05-10 17:20:33 +0300

    functionality of adding a recipe to a file, if user wants to is included.
    if user has allergy then the program filters them.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import requests
 
+
 def recipe_search(ingredient):
     app_id = "9a01e4df"
     app_key = "f55aa716d20221d7c0b1a7bd7d721c8c"
 
-    result = requests.get('https://api.edamam.com/search?q={}&app_id={}&app_key={}'.format(ingredient, app_id, app_key))
+    result = requests.get(f'https://api.edamam.com/search?q={ingredient}&app_id={app_id}&app_key={app_key}')
 
     if result.status_code == 200:
         data = result.json()
@@ -19,37 +20,49 @@ def recipe_search(ingredient):
         print("Status code:", result.status_code)
 
 
+def save_recipe_to_favourites():
+    # Ask if the user wants to save any recipe to favorites
+    liked_recipe = input("Is there any particular recipe that you liked among these recipes? "
+                         "Please respond with 'y' or 'n': ")
+
+    if liked_recipe.lower() == "y":
+        # Ask the name of the recipe
+        recipe_name = input("What is the name of the recipe? Please respond with the label of the recipe: ")
+
+        # Ask for the URI of the recipe to uniquely identify it
+        uri_of_the_fav_recipe = input("Please provide the uri of the recipe: ")
+
+        # Append the recipe name and URI to the file instead of rewriting the file each time the function runs
+        with open('favourite_recipes.txt', "a") as fav_file:
+            fav_file.write(f"{recipe_name}, {uri_of_the_fav_recipe}\n")
+
+        print(f"{recipe_name} has been added to your favourite recipes.")
+
+
 def run():
     ingredient = input('Enter an ingredient: ')
     results = recipe_search(ingredient)
 
-    # Exit the function if the status code is something else than 200
     if results is None:
         return
 
-    # Ask user if there is any ingredient that the user might be allergic to
-    has_allergy = input("Do you have any allergies? Please respond with 'yes' (y) or 'no' (n): ")
+    has_allergy = input("Do you have any allergies? Please respond with 'y' or 'n': ")
 
-    # If the user is allergic to some ingredient
-    if has_allergy == "y":
+    if has_allergy.lower() == "y":
         allergy = input("What are you allergic to? Please specify it below: ").lower()
         filtered_results = []
 
         for result in results:
             recipe = result["recipe"]
-
-            # Flattening the ingredients to lowercase
             ingredients = [ing.lower() for ing in recipe["ingredientLines"]]
-
-            # Filtering the recipes that might contain the allergen
             if not any(allergy in ingredient for ingredient in ingredients):
                 filtered_results.append(recipe)
 
+        # Show the recipes that are obtained from the API and also the recipes after being filtered.
         print("Number of original recipes:", len(results))
         print("Number of recipes after filtering:", len(filtered_results))
         print()
 
-        # Loop through the list of recipes that are filtered not containing the allergen
         for recipe in filtered_results:
             print("Recipe:", recipe["label"])
             print("URL:", recipe["uri"])
@@ -57,7 +70,6 @@ def run():
             print("Diet Labels:", ', '.join(recipe["dietLabels"]))
             print()
 
-    # If the user is NOT allergic to an ingredient
     else:
         for result in results:
             print("Recipe:", result["recipe"]["label"])
@@ -66,8 +78,9 @@ def run():
             print("Diet Labels:", ', '.join(result["recipe"]["dietLabels"]))
             print()
 
-    # Do you have any favourite recipe?
+    # Ask after displaying all recipes
+    save_recipe_to_favourites()
 
-    # Would you like to save a recipe to your favourite recipes?
 
 run()
+
